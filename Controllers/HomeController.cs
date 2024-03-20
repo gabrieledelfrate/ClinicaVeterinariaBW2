228b00bb1ed06f14507066a068f2d516a4510942
Lorenzo <lorenzo.zuccante@outlook.it>
2024-03-20 02:08:45 +0100

    Inizio farmacia : Login farmacia e farmacia index,details

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,18 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using ClinicaVeterinaria.Models;
 
 namespace ClinicaVeterinaria.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-
             return View();
         }
+
+        //Lorenzo-Daniel
+        public ActionResult LoginFarmacia()
+        {
+          
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken] 
+        public ActionResult LoginFarmacia(string email, string passwordFarmacista)
+        {
+          
+            using (var db = new DBContext())
+            {
+                var pharmacist = db.Pharmacists.FirstOrDefault(p => p.Email == email && p.PasswordFarmacista == passwordFarmacista);
+
+                if (pharmacist != null)
+                {
+                    FormsAuthentication.SetAuthCookie(email, false);
+                    return RedirectToAction("Index", "Pharmacists");
+                }
+                else
+                {
+                    ViewBag.Error = "Credenziali non valide";
+                    return View();
+                }
+            }
+        }
+
+        
+        public ActionResult LogoutFarmacia()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
+        }
+        //Lorenzo-Daniel
     }
 }
--- Controllers/PharmacistsController.cs
+++ Controllers/PharmacistsController.cs
@@ -1,17 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using ClinicaVeterinaria.Models;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace ClinicaVeterinaria.Controllers
 {
     public class PharmacistsController : Controller
     {
-        // GET: Pharmacists
-        public ActionResult Index()
+        private DBContext db = new DBContext();
+
+        
+        public ActionResult Index(string search)
         {
-            return View();
+           
+
+            IQueryable<Product> products = db.Products;
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                products = products.Where(s => s.Nome.Contains(search));
+            }
+
+            return View(products.ToList());
         }
+
+
+        public ActionResult Details(int id)
+        {
+            var product = db.Products
+                            .Include(p => p.Drawer)
+                            .Include(p => p.Drawer.Locker)
+                            .FirstOrDefault(p => p.ProductID == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
     }
 }
